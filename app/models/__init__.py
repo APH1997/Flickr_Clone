e248bd3b4f2f027092850fd8092827b3e4b8d2d1
Andre <andrehristu@gmail.com>
2023-05-16 13:49:04 -0400

    added reply model and connected to users, comments

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,3 +3,4 @@ from .db import environment, SCHEMA
 from .user import User
 from .photo import Photo
 from .comment import Comment
+from .reply import Reply
--- app/models/reply.py
+++ app/models/reply.py
@@ -1,37 +1,29 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 
-
-class Comment(db.Model):
-    __tablename__ = 'comments'
+class Reply(db.Model):
+    __tablename__ = 'replies'
 
     if environment == "production":
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
     author_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')))
-    photo_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('photos.id')))
+    parent_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('comments.id')))
     content = db.Column(db.String(200), nullable=False)
-
-    photo = db.relationship(
-        "Photo",
-        back_populates="comments"
-    )
 
     author = db.relationship(
         "User",
-        back_populates="comments"
+        back_populates=""
     )
 
-    replies = db.relationship(
-        "Reply",
-        back_populates="parent"
+    parent = db.relationship(
+        "Comment",
+        back_populates="replies"
     )
 
     def to_dict(self):
         return {
             'id': self.id,
             'author': self.author.to_dict(),
-            'photo': self.photo.to_dict_no_author(),
-            'content': self.content,
-            'replies': [reply.to_dict() for reply in self.replies]
+            'content': self.content
         }
