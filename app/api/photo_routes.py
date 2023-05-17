0e8aa9ec6d209fd450af5161f35057ab00a0c830
Andre <andrehristu@gmail.com>
2023-05-17 14:00:43 -0400

    post 1 photo route

--- app/api/photo_routes.py
+++ app/api/photo_routes.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from flask_login import login_required
-from app.models import Photo
+from app.models import db, Photo
+from forms import PhotoForm
+from app.api.aws_helpers import get_unique_filename, upload_file_to_s3, remove_file_from_s3
 
 photo_routes = Blueprint('photos', __name__)
 
@@ -49,7 +51,30 @@ def post_photo():
     If succesful, uploads to AWS and returns new photo.to_dict_no_author()
     If fail, return error dictionary
     """
-    
+    form = PhotoForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        photo = form.data["photo"]
+        photo.filename = get_unique_filename(photo.filename)
+        upload = upload_file_to_s3(photo)
+
+        if "url" not in upload:
+            return upload["errors"]
+
+        photo_url = upload["url"]
+
+        new_photo = Photo(
+            author_id = form.data["author_id"],
+            aws_url = photo_url,
+            caption = form.data["caption"],
+            description = form.data["description"]
+        )
+
+        db.session.add(new_photo)
+        db.session.commit()
+        return jsonify(new_photo.to_dict())
+    else:
+        return jsonify({"errors": form.errors})
 
 # Edit Photo by Id
 
