567723d8e74c55bc93818a9f288983a86380b946
Andre <andrehristu@gmail.com>
2023-05-17 12:09:24 -0400

    get photo routes done

--- app/api/photo_routes.py
+++ app/api/photo_routes.py
@@ -10,16 +10,46 @@ photo_routes = Blueprint('photos', __name__)
 def all_photos():
     """
     Query for all photos and returns them
-    in a list of dictionaries WITH authors and comments;
+    in a list of dictionaries WITH authors, comments, and albums;
     This route populates the home feed.
     """
     photos = Photo.query.all()
     return {'photos': [photo.to_dict() for photo in photos]}
+
+
 # Get All Photos by User Id
-# Get All Photos by Album Id
+@photo_routes.route('/user/<int:userId>')
+@login_required
+def all_user_photos(userId):
+    """
+    Query for all photos where author_id == userId
+    This route populates a user page's photo stream tab
+    """
+    photos = Photo.query.filter(Photo.author_id == userId).all()
+    return {'photos': [photo.to_dict() for photo in photos]}
+
+
 # Get Photo by Photo Id
+@photo_routes.route('/<int:photoId>')
+@login_required
+def get_photo_by_id(photoId):
+    """
+    Query for one photo by it's Id
+    Return in a dictionary with comments, albums, author
+    """
+    photo = Photo.query.get(photoId)
+    return photo.to_dict()
 
 # Post Photo
+@photo_routes.route('/new', methods=['POST'])
+@login_required
+def post_photo():
+    """
+    Takes form data, validates against FlaskForm
+    If succesful, uploads to AWS and returns new photo.to_dict_no_author()
+    If fail, return error dictionary
+    """
+    
 
 # Edit Photo by Id
 
