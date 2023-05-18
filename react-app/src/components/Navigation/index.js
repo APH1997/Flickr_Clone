d6334d33c5ea150c89a7bc48de2baea3c482c70b
Andre <andrehristu@gmail.com>
2023-05-18 19:45:29 -0400

    went sicko on the logo

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -4,13 +4,18 @@ import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
 
+
 function Navigation({ isLoaded }){
 	const history = useHistory()
 	const sessionUser = useSelector(state => state.session.user);
 
 	return (
 		<div className='nav-container'>
-			<div>
+			<div id="logo-container" onClick={() => history.push('/')}>
+
+				<NavLink id="cloud" to={'/'}>
+				<i className="fas fa-cloud"></i>
+				</NavLink>
 				<h1 id="home">high<span id="home-r">r</span>Me</h1>
 			</div>
 			{isLoaded && (
