567e21ae6607ad3b0b53de7f52fe5ccd5f79acb5
Andre <andrehristu@gmail.com>
2023-05-18 18:19:25 -0400

    styled home logo

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useHistory } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
 
 function Navigation({ isLoaded }){
+	const history = useHistory()
 	const sessionUser = useSelector(state => state.session.user);
 
 	return (
 		<div className='nav-container'>
 			<div>
-				<NavLink exact to="/">HighrMe</NavLink>
+				<h1 id="home">high<span id="home-r">r</span>Me</h1>
 			</div>
 			{isLoaded && (
 				<div>
