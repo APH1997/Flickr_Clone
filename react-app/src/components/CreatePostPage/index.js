2eb632520395cbcc76bc181a02ba5064595ee35f
Andre <andrehristu@gmail.com>
2023-05-18 12:33:54 -0400

    create photo frontend error handling

--- react-app/src/components/CreatePostPage/index.js
+++ react-app/src/components/CreatePostPage/index.js
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from "react-redux"
 import { useHistory } from "react-router-dom"
 import { useState, useEffect } from "react"
+import { createPhotoThunk } from "../../store/photos"
 
 
 function CreatePostForm() {
@@ -11,16 +12,51 @@ function CreatePostForm() {
     const [description, setDescription] = useState("")
     const [photo, setPhoto] = useState(null)
     const [photoPreview, setPhotoPreview] = useState(null)
+    const [errors, setErrors] = useState({})
 
     const handlePhotoChange = (e) => {
         setPhoto(e.target.files[0]);
         setPhotoPreview(URL.createObjectURL(e.target.files[0]))
     }
 
+    useEffect(() => {
+        const errObj = {}
+        if (!caption){
+            errObj.caption = "Please caption your photo."
+        }
+        if (caption.length > 100){
+            errObj.caption = "Caption must be less than 100 characters."
+        }
+        if (description && description.length > 500){
+            errObj.description = "Description must not exceed 500 characters."
+        }
+        if (!photo){
+            errObj.photo = "Please select a photo to upload"
+        }
+
+        if (Object.keys(errObj).length){
+            setErrors(errObj)
+        } else setErrors({})
+
+    }, [caption, description, photo])
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        if (Object.keys(errors).length) return;
+        alert("success!")
+
+
+    }
+
     return (
-        <form encType="multipart/form-data">
+        <form encType="multipart/form-data" onSubmit={handleSubmit}>
             <div>
                 <label>Caption</label>
+                {errors.caption &&
+                <p className="errors">{errors.caption}</p>
+                }
                 <input
                 type="text"
                 value={caption}
@@ -29,7 +65,10 @@ function CreatePostForm() {
             </div>
             <div>
                 <label>Description</label>
-                <input
+                {errors.description &&
+                <p className="errors">{errors.description}</p>
+                }
+                <textarea
                 type="text"
                 value={description}
                 onChange={(e) => setDescription(e.target.value)}
@@ -37,12 +76,17 @@ function CreatePostForm() {
             </div>
             <div>
                 <label>Upload Photo</label>
+                {errors.photo &&
+                <p className="errors">{errors.photo}</p>
+                }
                 <input
                 type="file"
                 accept="image/*"
                 onChange={handlePhotoChange}/>
                 <img src={photoPreview} style={{width:"100px", height:"100px"}}/>
             </div>
+
+            <button>Submit</button>
 
         </form>
     )
