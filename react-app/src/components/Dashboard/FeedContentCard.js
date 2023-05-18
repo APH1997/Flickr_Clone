affa9f445231a3c70b15c6f52fa4f706c9accef0
Andre <andrehristu@gmail.com>
2023-05-18 16:00:17 -0400

    delete functionality for posts; photo CRUD complete

--- react-app/src/components/Dashboard/FeedContentCard.js
+++ react-app/src/components/Dashboard/FeedContentCard.js
@@ -2,6 +2,7 @@ import { useSelector } from "react-redux"
 import { NavLink } from "react-router-dom"
 import OpenModalButton from "../OpenModalButton"
 import { useModal } from "../../context/Modal"
+import DeletePostModal from "../Photos/DeletePostModal"
 
 function ContentCard({photo}){
     const user = useSelector(state => state.session.user)
@@ -14,7 +15,10 @@ function ContentCard({photo}){
                     <p>{photo.author.username}</p>
                 </div>
                 {user.id === photo.author.id &&
-
+                        <OpenModalButton
+                        buttonText="Delete"
+                        modalComponent={<DeletePostModal photoId={photo.id}/>} />}
+                {user.id === photo.author.id  &&
                         <NavLink to={`/photos/${photo.id}/edit`}>Edit</NavLink>
                 }
             </div>
