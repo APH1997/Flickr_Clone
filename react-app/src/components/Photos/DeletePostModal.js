8982c972ed6fc04cd2d8ca6167c85d1ff97c18f1
Andre <andrehristu@gmail.com>
2023-05-20 14:06:48 -0400

    refresh session store on photo delete

--- react-app/src/components/Photos/DeletePostModal.js
+++ react-app/src/components/Photos/DeletePostModal.js
@@ -1,14 +1,23 @@
 import { useDispatch } from "react-redux"
 import { useModal } from "../../context/Modal"
 import { deletePhotoThunk } from "../../store/photos"
+import { useState } from "react"
+import { authenticate } from "../../store/session"
 
 
 function DeletePostModal({photoId}){
     const { closeModal } = useModal()
     const dispatch = useDispatch()
+    const [isDeleting, setIsDeleting] = useState(false)
 
     const handleYes = async () => {
+        setIsDeleting(true)
         await dispatch(deletePhotoThunk(photoId))
+
+        //update session store
+        setTimeout(() => dispatch(authenticate()), 1000)
+        setTimeout(() => setIsDeleting(false), 1000)
+
         closeModal()
     }
 
@@ -16,8 +25,8 @@ function DeletePostModal({photoId}){
     return (
         <div>
             <h2>Are you sure you want to remove this post?</h2>
-            <button onClick={handleYes}>Yes</button>
-            <button onClick={() => closeModal()}>No</button>
+            <button onClick={handleYes}>{isDeleting ? "Deleting..." : "Yes"}</button>
+            <button onClick={() => closeModal()} disabled={isDeleting}>No</button>
         </div>
     )
 }
