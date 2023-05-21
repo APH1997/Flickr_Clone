fcf2427dcacf36bfe23afcb4adec00929b890268
Andre <andrehristu@gmail.com>
2023-05-21 12:57:45 -0400

    connected user page album card to album show

--- react-app/src/components/UserPage/AlbumCard.js
+++ react-app/src/components/UserPage/AlbumCard.js
@@ -1,12 +1,15 @@
 import { useSelector } from "react-redux"
+import { useHistory } from "react-router-dom";
 
 function AlbumCard({album}){
     const pageOwner = useSelector(state => state.session.profilePageUser)
-    
+    const history = useHistory()
+
     if (!album) return null;
     return (
 
-            <div className="album-card-container">
+            <div className="album-card-container"
+                onClick={() => history.push(`/albums/${album.id}`)}>
                 <img id="album-cover-photo"src={album.cover_photo}></img>
                 <div className="album-card-details">
                     <p className="album-cover-album-title">{album.title}</p>
