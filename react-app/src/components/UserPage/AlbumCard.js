09e86fd2652e8f523cdb94dfa4f5a95524abd56b
Andre <andrehristu@gmail.com>
2023-05-21 13:34:03 -0400

    restyled album card in user profile

--- react-app/src/components/UserPage/AlbumCard.js
+++ react-app/src/components/UserPage/AlbumCard.js
@@ -10,11 +10,11 @@ function AlbumCard({album}){
 
             <div className="album-card-container"
                 onClick={() => history.push(`/albums/${album.id}`)}>
-                <img id="album-cover-photo"src={album.cover_photo}></img>
                 <div className="album-card-details">
                     <p className="album-cover-album-title">{album.title}</p>
                     <p className="album-cover-author">{album.length} photos</p>
                 </div>
+                <img id="album-cover-photo"src={album.cover_photo}></img>
             </div>
 
     )
