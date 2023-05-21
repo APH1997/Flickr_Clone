4e08ace4ded78feb1c865c01679220b2a10f1935
Andre <andrehristu@gmail.com>
2023-05-21 12:54:33 -0400

    album show looking good

--- react-app/src/components/Albums/index.js
+++ react-app/src/components/Albums/index.js
@@ -1,8 +1,9 @@
 import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
-import { useParams } from "react-router-dom"
+import { NavLink, useParams } from "react-router-dom"
 import { getAllAlbumsThunk } from "../../store/albums"
 import "./index.css"
+
 function AlbumShow(){
     const {albumId} = useParams()
     const dispatch = useDispatch()
@@ -21,7 +22,20 @@ function AlbumShow(){
     return (
         <>
             <div className="album-photos-container">
-                <img id="album-show-cover-photo" src={singleAlbum.cover_photo}></img>
+                <div>
+                    <div id="cover-photo-mask" style={{width:"80%", height:"250px"}}>
+                        <div id="cover-photo-mask-details">
+                            <div id="title-and-photo-count">
+                                <h4>{singleAlbum.title}</h4>
+                                <p>{singleAlbum.pics.length} photos</p>
+                            </div>
+                            <NavLink id="album-show-nav-link-to-author" to={`/users/${singleAlbum.author.id}`}>
+                                By: {singleAlbum.author.first_name} {singleAlbum.author.last_name}
+                            </NavLink>
+                        </div>
+                    </div>
+                    <img id="album-show-cover-photo" src={singleAlbum.cover_photo}></img>
+                </div>
                 <div className="non-cover-photos">
                     {singleAlbum.pics.map((photo) =>
                         <div className="album-photo-card">
