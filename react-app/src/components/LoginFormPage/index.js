886ad7019ae120bbbfc136a529decc01f527905c
Andre <andrehristu@gmail.com>
2023-05-22 12:36:51 -0400

    connect signup to splash page; demo user functionality

--- react-app/src/components/LoginFormPage/index.js
+++ react-app/src/components/LoginFormPage/index.js
@@ -27,10 +27,25 @@ function LoginFormPage() {
     }
   };
 
+  const handleDemo = async(e) => {
+    const data = await dispatch(login('demo@aa.io', 'password'));
+    if (data) {
+      setErrors(data)
+    }
+  }
+
+  const signupRedirect = () => {
+    history.push('/signup')
+  }
+
   return (
     <div className="login-form-page-container">
       <form id="login-form-fields"onSubmit={handleSubmit}>
       <h1>Log In</h1>
+      <p>Don't have an account?
+        <span> <span onClick={() => signupRedirect()} className="no-account-options">Sign up</span> </span>
+        or log in as a <span className="no-account-options" onClick={() => handleDemo()}>Demo User!</span>
+      </p>
         <ul>
           {errors.map((error, idx) => (
             <li key={idx}>{error}</li>
