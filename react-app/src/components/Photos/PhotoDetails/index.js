b32f7a007438377eb1cb13ab5ac14688397ef9d9
Andre <andrehristu@gmail.com>
2023-05-22 19:51:07 -0400

    top half of photo show looks goood

--- react-app/src/components/Photos/PhotoDetails/index.js
+++ react-app/src/components/Photos/PhotoDetails/index.js
@@ -1,14 +1,36 @@
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { useParams } from "react-router-dom"
+import { useEffect } from "react"
+import { getAllPhotosThunk } from "../../../store/photos"
+import "./index.css"
 
 function PhotoDetails(){
+    const dispatch = useDispatch()
     const {photoId} = useParams()
-    const photo = useSelector((state) => state.photos.allPhotos[photoId])
+    const allPhotos = useSelector((state) => state.photos.allPhotos)
 
-    console.log('PHOTODETAILS PHOTO:', photo)
 
+    useEffect(() => {
+        dispatch(getAllPhotosThunk())
+    },[dispatch])
+
+    if (!allPhotos) return null;
+    const photo = allPhotos[photoId];
     return (
-        <h1>Welcome to da photo show</h1>
+        <div>
+            <div className="big-black-background-div">
+                <div id="breadcrumb-to-dash">
+                    <i class="fas fa-arrow-left"></i>
+                    <span>Back to feed</span>
+                </div>
+                <div><img src={photo.url}></img></div>
+                <div>author controls</div>
+            </div>
+            <h2>Second div is a column of all comments</h2>
+            <div className="dark-div">
+                <h1>Make a comment card to map</h1>
+            </div>
+        </div>
     )
 }
 
