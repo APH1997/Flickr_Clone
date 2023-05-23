f5b9a04e5834bbfcc319955a64057557bddd82c9
Andre <andrehristu@gmail.com>
2023-05-23 13:34:36 -0400

    user page photo show styled roughly

--- react-app/src/components/UserPage/PhotoShow.js
+++ react-app/src/components/UserPage/PhotoShow.js
@@ -4,7 +4,7 @@ import { useHistory } from "react-router-dom"
 function PhotoShow({photo}){
     const history = useHistory()
     return (
-        <div onClick={() => history.push(`/photos/${photo.id}`)}>
+        <div className="photo-show-card" onClick={() => history.push(`/photos/${photo.id}`)}>
             <img alt="" src={photo.url}></img>
         </div>
     )
--- react-app/src/components/UserPage/ProfileNav/PhotosTab.js
+++ react-app/src/components/UserPage/ProfileNav/PhotosTab.js
@@ -2,6 +2,8 @@ import { useSelector } from "react-redux"
 import PhotoShow from "../PhotoShow";
 import { useState } from "react";
 import PostForm from "../../Photos/CreatePostPage";
+import { useModal } from "../../../context/Modal";
+import OpenModalButton from "../../OpenModalButton";
 
 function PhotosTab(){
     const [uploadComponent, setUploadComponent] = useState(false)
@@ -14,10 +16,15 @@ function PhotosTab(){
     return (
         <div className="user-page-photo-container">
             {user.id === pageOwner.id &&
-                <div onClick={() => setUploadComponent(!uploadComponent)}>Upload a photo</div>
+                <div className="upload-photo-user-page-container">
+                    <OpenModalButton
+                    buttonText="Upload a photo"
+                    modalComponent={<PostForm />}
+                    />
+                </div>
             }
 
-            <div>
+            <div className="photo-show-container">
                 {!uploadComponent &&
                 photos.map(photo => {
                     return <PhotoShow id={photo.id} photo={photo}/>
