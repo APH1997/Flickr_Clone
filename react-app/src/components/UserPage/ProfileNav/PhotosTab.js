dde1284cfe2fbf7992bdbf4517815434d31f0f7e
Andre <andrehristu@gmail.com>
2023-05-23 11:44:39 -0400

    conditionally render upload photo form in user page photo tab

--- react-app/src/components/UserPage/ProfileNav/PhotosTab.js
+++ react-app/src/components/UserPage/ProfileNav/PhotosTab.js
@@ -1,17 +1,30 @@
 import { useSelector } from "react-redux"
 import PhotoShow from "../PhotoShow";
+import { useState } from "react";
+import PostForm from "../../Photos/CreatePostPage";
 
 function PhotosTab(){
-    const pageOwner = useSelector(state => state.session.profilePageUser)
+    const [uploadComponent, setUploadComponent] = useState(false)
 
+    const pageOwner = useSelector(state => state.session.profilePageUser)
+    const user = useSelector(state => state.session.user)
     if (!pageOwner) return null;
     const photos = pageOwner.photos
 
     return (
         <div className="user-page-photo-container">
-            {photos.map(photo => {
+            {user.id === pageOwner.id &&
+                <div onClick={() => setUploadComponent(!uploadComponent)}>Upload a photo</div>
+            }
+            {!uploadComponent &&
+            photos.map(photo => {
                 return <PhotoShow id={photo.id} photo={photo}/>
+
             })}
+            {uploadComponent &&
+                <PostForm />
+            }
+
         </div>
     )
 }
