e30afe7c5221ff8966b5acc2698684d441b6743b
Andre <andrehristu@gmail.com>
2023-05-22 20:21:40 -0400

    finished top half of photo page

--- react-app/src/components/Photos/PhotoDetails/index.js
+++ react-app/src/components/Photos/PhotoDetails/index.js
@@ -3,13 +3,14 @@ import { useHistory, useParams } from "react-router-dom"
 import { useEffect } from "react"
 import { getAllPhotosThunk } from "../../../store/photos"
 import "./index.css"
+import AuthorControls from "../../Dashboard/AuthorControls"
 
 function PhotoDetails(){
     const history = useHistory()
     const dispatch = useDispatch()
     const {photoId} = useParams()
     const allPhotos = useSelector((state) => state.photos.allPhotos)
-
+    const user = useSelector((state) => state.session.user)
 
     useEffect(() => {
         dispatch(getAllPhotosThunk())
@@ -29,7 +30,11 @@ function PhotoDetails(){
                 <div id="main-photo-container">
                     <img id="photo-show-main-img"src={photo.url}></img>
                 </div>
-                <div>author controls</div>
+                <div className="specific" id="author-controls-container">
+                    {user.id === photo.author.id &&
+                        <AuthorControls photo={photo}/>
+                    }
+                </div>
             </div>
             <h2>Second div is a column of all comments</h2>
             <div className="dark-div">
