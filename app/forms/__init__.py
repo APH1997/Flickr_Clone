06e236db14daef12953389c954d9c62a833e67d0
Andre <andrehristu@gmail.com>
2023-05-23 23:27:18 -0400

    backend route for editing user

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -2,3 +2,4 @@ from .login_form import LoginForm
 from .signup_form import SignUpForm
 from .photos import PhotoForm, EditPhotoForm
 from .albums import CreateAlbumForm, EditAlbumForm
+from .edit_user_form import EditUserForm
