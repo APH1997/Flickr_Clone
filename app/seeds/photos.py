efe94caea2007531f1bc63077b009e288c10bd05
Andre <andrehristu@gmail.com>
2023-05-27 17:25:38 -0400

    space pics added

--- app/seeds/photos.py
+++ app/seeds/photos.py
@@ -44,13 +44,73 @@ def seed_photos():
     )
     space3 = Photo(
         author_id=3,
-        aws_url='https://highrme-pics.s3.us-east-2.amazonaws.com/WR+124.png',
-        caption='Wolf-Rayet 124',
-        description="The luminous, hot star Wolf-Rayet 124 (WR 124) is prominent at the centre of the NASA/ESA/CSA James Webb Space Telescope's composite image combining near-infrared and mid-infrared wavelengths of light.",
+        aws_url='https://highrme-pics.s3.us-east-2.amazonaws.com/sh2.jpg',
+        caption='Sh2-265',
+        description='Sh2-265 is a very dimmed reddish H-alpha diffused nebula spread around 2 degrees northwest of Bellatrix,  and has a span of about 50 arc minutes.',
+        created_at=fake.date_between(start_date='-1y', end_date='today'),
+    )
+    space4 = Photo(
+        author_id=3,
+        aws_url='https://highrme-pics.s3.us-east-2.amazonaws.com/pinwheel.jpg',
+        caption='Pinwheel Galaxy',
+        description="A little over 4 hour integration I did from a Bortle Class 4-5. The supernova is shining brilliantly at the 11 o'clock position of the Pinwheel Galaxy.",
+        created_at=fake.date_between(start_date='-1y', end_date='today'),
+    )
+    space5 = Photo(
+        author_id=3,
+        aws_url='https://highrme-pics.s3.us-east-2.amazonaws.com/wildduckcluster.jpg',
+        caption='M11: the Wild Duck Cluster',
+        description='The Wild Duck Cluster is one of the richest and most compact of the known open clusters. It is one of the most massive open clusters known, and it has been extensively studied. Its age has been estimated to about 316 million years.',
+        created_at=fake.date_between(start_date='-1y', end_date='today'),
+    )
+    space6 = Photo(
+        author_id=3,
+        aws_url='https://highrme-pics.s3.us-east-2.amazonaws.com/MilkyWay.jpg',
+        caption='MilkyWay',
+        description='Taken last summer from the Catskills in Upstate NY. ~28 min exposure processed in Sequator and Photoshop.',
+        created_at=fake.date_between(start_date='-1y', end_date='today'),
+    )
+    space7 = Photo(
+        author_id=3,
+        aws_url='https://highrme-pics.s3.us-east-2.amazonaws.com/pillarsofcreation.jpg',
+        caption='Pillars of Creation',
+        description='The Pillars of Creation are bathed in the blistering ultraviolet light from a grouping of young, massive stars located off the top of the image. Streamers of gas can be seen bleeding off the pillars as the intense radiation heats and evaporates it into space. Denser regions of the pillars are shadowing material beneath them from the powerful radiation. Stars are being born deep inside the pillars, which are made of cold hydrogen gas laced with dust.',
+        created_at=fake.date_between(start_date='-1y', end_date='today'),
+    )
+    space8 = Photo(
+        author_id=3,
+        aws_url='https://highrme-pics.s3.us-east-2.amazonaws.com/ngc5189.jpg',
+        caption='NGC 5189',
+        description="NASA's Hubble Space Telescope photographed a nearby planetary nebula called NGC 5189. The intricate structure of this bright gaseous nebula resembles a glass-blown holiday ornament with a glowing ribbon entwined. Planetary nebulae represent the final brief stage in the life of a medium-sized star like our Sun. While consuming the last of the fuel in its core, the dying star expels a large portion of its outer envelope.",
+        created_at=fake.date_between(start_date='-1y', end_date='today'),
+    )
+    space9 = Photo(
+        author_id=3,
+        aws_url='https://highrme-pics.s3.us-east-2.amazonaws.com/eagle_nebula.jpg',
+        caption="Eagle Nebula",
+        description='What do you get when you combine X-ray, infrared, and optical data? These gorgeous views showcase star cluster NGC 346, spiral galaxies NGC 1672 and Messier 74, and the Pillars of Creation (Eagle Nebula) in ways we could never see with just our eyes. The composite images here combine data from the Webb, Chandra, Hubble, Spitzer, XMM-Newton, and ESO telescopes.',
+        created_at=fake.date_between(start_date='-1y', end_date='today'),
+    )
+    space10 = Photo(
+        author_id=3,
+        aws_url='https://highrme-pics.s3.us-east-2.amazonaws.com/carinanebula.jpg',
+        caption='Carina Nebula',
+        description='You might remember this view, taken in the Carina Nebula, as part of Webb’s first images released. A deep dive into the data has newly revealed a hotbed of young stars in an elusive stage of development. This discovery may help us investigate how stars like our Sun form, as well as how radiation from nearby massive stars might affect the development of planets.',
+        created_at=fake.date_between(start_date='-1y', end_date='today'),
+    )
+    space11 = Photo(
+        author_id=3,
+        aws_url='https://highrme-pics.s3.us-east-2.amazonaws.com/cosmicreef.jpg',
+        caption='Cosmic Reef',
+        description='The sparkling centerpiece of NGC 2014 is a grouping of bright, hefty stars, each 10 to 20 times more massive than our Sun. The stars’ ultraviolet radiation heats the surrounding dense gas and unleash fierce winds of charged particles that blast away lower-density gas, forming the bubble-like structures seen on the right. The blue areas in NGC 2014 reveal the glow of oxygen, heated to nearly 20,000 degrees Fahrenheit by the blast of ultraviolet light.',
         created_at=fake.date_between(start_date='-1y', end_date='today'),
     )
 
-    all_pics = [sunset1, sunset2, sunset3, space1, space2, space3]
+
+    all_pics = [sunset1, sunset2, sunset3,
+                space1, space2, space3, space4,
+                space5, space6, space7, space8,
+                space9, space10, space11]
 
     added = [db.session.add(pic) for pic in all_pics]
     db.session.commit()
--- app/seeds/users.py
+++ app/seeds/users.py
@@ -10,13 +10,28 @@ def seed_users():
         username='SunsetSnapper', first_name='Marnothy', last_name='Glumpford IV', email='marnie@aa.io', password='password')
     bobbie = User(
         username='GalacticGawker', first_name='Robert', last_name='Szabo', email='bobbie@aa.io', password='password')
+    luna = User(
+        username='LunarLunatic', first_name='Luthor', last_name='Ludlowe', email='luthor@aa.io', password='password')
+    padding1 = User(
+        username='Gribble419', first_name='Gary', last_name='Song', email='gary@aa.io', password='password')
+    padding2 = User(
+        username='HubbleBubbleTrubble', first_name='Dirk', last_name='Samson', email='dirk@aa.io', password='password')
+    padding3 = User(
+        username='xXDragonGodXx68', first_name='Dorian', last_name='Macias', email='dorian@aa.io', password='password')
+    padding4 = User(
+        username='ButterBallBandit', first_name='Skylar', last_name='Biggums', email='skylar@aa.io', password='password')
+    padding5 = User(
+        username='SnatchMan', first_name='Ron', last_name='Bossman', email='ron@aa.io', password='password')
+    padding6 = User(
+        username='GlueEater1122', first_name='Gloria', last_name='Kaufmann', email='gloria@aa.io', password='password')
 
-    db.session.add(demo)
-    db.session.add(marnie)
-    db.session.add(bobbie)
+    allUsers = [demo, marnie, bobbie, luna, padding1, padding2,
+                padding3, padding4, padding5, padding6]
+    addAll = [db.session.add(user) for user in allUsers]
     db.session.commit()
+    
+    return allUsers
 
-    return [demo, marnie, bobbie]
 # Uses a raw SQL query to TRUNCATE or DELETE the users table. SQLAlchemy doesn't
 # have a built in function to do this. With postgres in production TRUNCATE
 # removes all the data from the table, and RESET IDENTITY resets the auto
