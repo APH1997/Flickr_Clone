4de44881e45f415b6d2f210aab18051c667b0ec6
Andre <andrehristu@gmail.com>
2023-05-30 19:59:36 -0400

    db model and random seed for photo likes done

--- app/seeds/photos.py
+++ app/seeds/photos.py
@@ -2,7 +2,7 @@ from app.models import db, Photo, environment, SCHEMA
 from sqlalchemy.sql import text
 from faker import Faker
 from datetime import date
-from random import sample, choice
+from random import sample, choice, randint
 fake = Faker()
 
 
@@ -13,7 +13,7 @@ def seed_photos(all_users):
         caption='Caught this beaut on the way to Zumba LOL',
         description='Sunsets cast a serene ambiance as the sun dips below the horizon, painting the sky in a vivid display of colors.',
         created_at=fake.date_between(start_date='-1y', end_date='today'),
-        user_likes = sample[all_users, choice(range(0, 11))]
+        user_likes = sample(all_users, randint(0, 10))
     )
     sunset2 = Photo(
         author_id=2,
@@ -21,7 +21,7 @@ def seed_photos(all_users):
         caption='Pastel Sunset',
         description='',
         created_at=fake.date_between(start_date='-1y', end_date='today'),
-        user_likes = sample[all_users, choice(range(0, 11))]
+        user_likes = sample(all_users, randint(0, 10))
     )
     sunset3 = Photo(
         author_id=2,
@@ -29,7 +29,7 @@ def seed_photos(all_users):
         caption='Crispy Clouds Before Sunset',
         description='',
         created_at=fake.date_between(start_date='-1y', end_date='today'),
-        user_likes = sample[all_users, choice(range(0, 11))]
+        user_likes = sample(all_users, randint(0, 10))
     )
     sunset4 = Photo(
         author_id=2,
@@ -37,7 +37,7 @@ def seed_photos(all_users):
         caption='I love sunsets :)',
         description="The breathtaking hues of a sunset emerge from the scattering of light particles in the Earth's atmosphere.",
         created_at=fake.date_between(start_date='-1y', end_date='today'),
-        user_likes = sample[all_users, choice(range(0, 11))],
+        user_likes = sample(all_users, randint(0, 10)),
     )
     sunset5 = Photo(
         author_id=2,
@@ -45,7 +45,7 @@ def seed_photos(all_users):
         caption='Dusty Dusk',
         description='Coastal regions boast mesmerizing sunsets, with the interplay of water reflecting and intensifying the radiant colors.',
         created_at=fake.date_between(start_date='-1y', end_date='today'),
-        user_likes = sample[all_users, choice(range(0, 11))],
+        user_likes = sample(all_users, randint(0, 10)),
     )
     sunset6 = Photo(
         author_id=2,
@@ -53,7 +53,7 @@ def seed_photos(all_users):
         caption='Woke up like this',
         description='The duration of a sunset varies depending on the time of year and geographical location, creating unique experiences.',
         created_at=fake.date_between(start_date='-1y', end_date='today'),
-        user_likes = sample[all_users, choice(range(0, 11))],
+        user_likes = sample(all_users, randint(0, 10)),
     )
     sunset7 = Photo(
         author_id=2,
@@ -61,7 +61,7 @@ def seed_photos(all_users):
         caption='Deep Run Sun',
         description='Sunsets evoke a sense of tranquility, inviting us to pause, reflect, and appreciate the beauty of the natural world.',
         created_at=fake.date_between(start_date='-1y', end_date='today'),
-        user_likes = sample[all_users, choice(range(0, 11))],
+        user_likes = sample(all_users, randint(0, 10)),
     )
 
     space1 = Photo(
@@ -70,7 +70,7 @@ def seed_photos(all_users):
         caption="Hubble's New View of the Tarantula Nebula",
         description="The Tarantula Nebula is a large star-forming region of ionized hydrogen gas that lies 161,000 light-years from Earth in the Large Magellanic Cloud, and its turbulent clouds of gas and dust appear to swirl between the region's bright, newly formed stars.",
         created_at=fake.date_between(start_date='-1y', end_date='today'),
-        user_likes = sample[all_users, choice(range(0, 11))],
+        user_likes = sample(all_users, randint(0, 10)),
     )
     space2 = Photo(
         author_id=3,
@@ -78,7 +78,7 @@ def seed_photos(all_users):
         caption='Hubble Popped Off',
         description='The Andromeda Galaxy (also known as Messier 31, M31, NGC 224 and originally the Andromeda Nebula) is a barred spiral galaxy and is the closest major galaxy to the Milky Way',
         created_at=fake.date_between(start_date='-1y', end_date='today'),
-        user_likes = sample[all_users, choice(range(0, 11))],
+        user_likes = sample(all_users, randint(0, 10)),
     )
     space3 = Photo(
         author_id=3,
@@ -86,7 +86,7 @@ def seed_photos(all_users):
         caption='Sh2-265',
         description='Sh2-265 is a very dimmed reddish H-alpha diffused nebula spread around 2 degrees northwest of Bellatrix,  and has a span of about 50 arc minutes.',
         created_at=fake.date_between(start_date='-1y', end_date='today'),
-        user_likes = sample[all_users, choice(range(0, 11))],
+        user_likes = sample(all_users, randint(0, 10)),
     )
     space4 = Photo(
         author_id=3,
@@ -94,7 +94,7 @@ def seed_photos(all_users):
         caption='Pinwheel Galaxy',
         description="A little over 4 hour integration I did from a Bortle Class 4-5. The supernova is shining brilliantly at the 11 o'clock position of the Pinwheel Galaxy.",
         created_at=fake.date_between(start_date='-1y', end_date='today'),
-        user_likes = sample[all_users, choice(range(0, 11))],
+        user_likes = sample(all_users, randint(0, 10)),
     )
     space5 = Photo(
         author_id=3,
@@ -102,7 +102,7 @@ def seed_photos(all_users):
         caption='M11: the Wild Duck Cluster',
         description='The Wild Duck Cluster is one of the richest and most compact of the known open clusters. It is one of the most massive open clusters known, and it has been extensively studied. Its age has been estimated to about 316 million years.',
         created_at=fake.date_between(start_date='-1y', end_date='today'),
-        user_likes = sample[all_users, choice(range(0, 11))],
+        user_likes = sample(all_users, randint(0, 10)),
     )
     space6 = Photo(
         author_id=3,
@@ -110,7 +110,7 @@ def seed_photos(all_users):
         caption='MilkyWay',
         description='Taken last summer from the Catskills in Upstate NY. ~28 min exposure processed in Sequator and Photoshop.',
         created_at=fake.date_between(start_date='-1y', end_date='today'),
-        user_likes = sample[all_users, choice(range(0, 11))],
+        user_likes = sample(all_users, randint(0, 10)),
     )
     space7 = Photo(
         author_id=3,
@@ -118,7 +118,7 @@ def seed_photos(all_users):
         caption='Pillars of Creation',
         description='The Pillars of Creation are bathed in the blistering ultraviolet light from a grouping of young, massive stars located off the top of the image. Streamers of gas can be seen bleeding off the pillars as the intense radiation heats and evaporates it into space. Denser regions of the pillars are shadowing material beneath them from the powerful radiation. Stars are being born deep inside the pillars, which are made of cold hydrogen gas laced with dust.',
         created_at=fake.date_between(start_date='-1y', end_date='today'),
-        user_likes = sample[all_users, choice(range(0, 11))],
+        user_likes = sample(all_users, randint(0, 10)),
     )
     space8 = Photo(
         author_id=3,
@@ -126,7 +126,7 @@ def seed_photos(all_users):
         caption='NGC 5189',
         description="NASA's Hubble Space Telescope photographed a nearby planetary nebula called NGC 5189. The intricate structure of this bright gaseous nebula resembles a glass-blown holiday ornament with a glowing ribbon entwined. Planetary nebulae represent the final brief stage in the life of a medium-sized star like our Sun. While consuming the last of the fuel in its core, the dying star expels a large portion of its outer envelope.",
         created_at=fake.date_between(start_date='-1y', end_date='today'),
-        user_likes = sample[all_users, choice(range(0, 11))],
+        user_likes = sample(all_users, randint(0, 10)),
     )
     space9 = Photo(
         author_id=3,
@@ -134,7 +134,7 @@ def seed_photos(all_users):
         caption="Eagle Nebula",
         description='What do you get when you combine X-ray, infrared, and optical data? These gorgeous views showcase star cluster NGC 346, spiral galaxies NGC 1672 and Messier 74, and the Pillars of Creation (Eagle Nebula) in ways we could never see with just our eyes. The composite images here combine data from the Webb, Chandra, Hubble, Spitzer, XMM-Newton, and ESO telescopes.',
         created_at=fake.date_between(start_date='-1y', end_date='today'),
-        user_likes = sample[all_users, choice(range(0, 11))],
+        user_likes = sample(all_users, randint(0, 10)),
     )
     space10 = Photo(
         author_id=3,
@@ -142,7 +142,7 @@ def seed_photos(all_users):
         caption='Carina Nebula',
         description='You might remember this view, taken in the Carina Nebula, as part of Webb’s first images released. A deep dive into the data has newly revealed a hotbed of young stars in an elusive stage of development. This discovery may help us investigate how stars like our Sun form, as well as how radiation from nearby massive stars might affect the development of planets.',
         created_at=fake.date_between(start_date='-1y', end_date='today'),
-        user_likes = sample[all_users, choice(range(0, 11))],
+        user_likes = sample(all_users, randint(0, 10)),
     )
     space11 = Photo(
         author_id=3,
@@ -150,7 +150,7 @@ def seed_photos(all_users):
         caption='Cosmic Reef',
         description='The sparkling centerpiece of NGC 2014 is a grouping of bright, hefty stars, each 10 to 20 times more massive than our Sun. The stars’ ultraviolet radiation heats the surrounding dense gas and unleash fierce winds of charged particles that blast away lower-density gas, forming the bubble-like structures seen on the right. The blue areas in NGC 2014 reveal the glow of oxygen, heated to nearly 20,000 degrees Fahrenheit by the blast of ultraviolet light.',
         created_at=fake.date_between(start_date='-1y', end_date='today'),
-        user_likes = sample[all_users, choice(range(0, 11))],
+        user_likes = sample(all_users, randint(0, 10)),
     )
 
     cloud1 = Photo(
@@ -159,7 +159,7 @@ def seed_photos(all_users):
         caption="Found Some Clouds",
         description="Puffy and white, they resemble cotton balls floating in the sky.",
         created_at=fake.date_between(start_date='-1y', end_date='today'),
-        user_likes = sample[all_users, choice(range(0, 11))],
+        user_likes = sample(all_users, randint(0, 10)),
     )
     cloud2 = Photo(
         author_id=5,
@@ -167,7 +167,7 @@ def seed_photos(all_users):
         caption="Oh look, More Clouds!",
         description=" Low and featureless, they create a gray, blanket-like cover overhead.",
         created_at=fake.date_between(start_date='-1y', end_date='today'),
-        user_likes = sample[all_users, choice(range(0, 11))],
+        user_likes = sample(all_users, randint(0, 10)),
     )
     cloud3 = Photo(
         author_id=5,
@@ -175,7 +175,7 @@ def seed_photos(all_users):
         caption="Love the Sky",
         description="Delicate and wispy, they form high in the atmosphere, resembling feathery streaks.",
         created_at=fake.date_between(start_date='-1y', end_date='today'),
-        user_likes = sample[all_users, choice(range(0, 11))],
+        user_likes = sample(all_users, randint(0, 10)),
     )
     cloud4 = Photo(
         author_id=5,
@@ -183,7 +183,7 @@ def seed_photos(all_users):
         caption="Clouds",
         description="Dark and ominous, they bring rain or storms, casting a gloomy atmosphere.",
         created_at=fake.date_between(start_date='-1y', end_date='today'),
-        user_likes = sample[all_users, choice(range(0, 11))],
+        user_likes = sample(all_users, randint(0, 10)),
     )
     cloud5 = Photo(
         author_id=5,
@@ -191,7 +191,7 @@ def seed_photos(all_users):
         caption="Look out!",
         description="Mid-level and lumpy, they create a mottled pattern with shades of gray and white.",
         created_at=fake.date_between(start_date='-1y', end_date='today'),
-        user_likes = sample[all_users, choice(range(0, 11))],
+        user_likes = sample(all_users, randint(0, 10)),
     )
     cloud6 = Photo(
         author_id=5,
@@ -199,7 +199,7 @@ def seed_photos(all_users):
         caption="Lorum Ipsum",
         description="Small, high-level clouds that resemble a field of tiny cotton balls.",
         created_at=fake.date_between(start_date='-1y', end_date='today'),
-        user_likes = sample[all_users, choice(range(0, 11))],
+        user_likes = sample(all_users, randint(0, 10)),
     )
     cloud7 = Photo(
         author_id=5,
@@ -207,7 +207,7 @@ def seed_photos(all_users):
         caption="Woah, Check out this Cloud!",
         description="Towering and majestic, they can reach great heights, producing thunderstorms and lightning.",
         created_at=fake.date_between(start_date='-1y', end_date='today'),
-        user_likes = sample[all_users, choice(range(0, 11))],
+        user_likes = sample(all_users, randint(0, 10)),
     )
 
     moon1 = Photo(
@@ -216,7 +216,7 @@ def seed_photos(all_users):
         caption="Moon and Venus",
         description="On the evening of April 23, 2023, in Yuzhno-Morskoy (Primorsky Krai), I photographed the conjunction of the crescent Moon and Venus. I used my camera Canon EOS 60D (with 135mm lens).",
         created_at=fake.date_between(start_date='-1y', end_date='today'),
-        user_likes = sample[all_users, choice(range(0, 11))],
+        user_likes = sample(all_users, randint(0, 10)),
     )
     moon2 = Photo(
         author_id=4,
@@ -224,7 +224,7 @@ def seed_photos(all_users):
         caption="Waxing Gibbous",
         description="Tonight's 34 percent illuminated waxing gibbous moon",
         created_at=fake.date_between(start_date='-1y', end_date='today'),
-        user_likes = sample[all_users, choice(range(0, 11))],
+        user_likes = sample(all_users, randint(0, 10)),
     )
     moon3 = Photo(
         author_id=4,
@@ -232,7 +232,7 @@ def seed_photos(all_users):
         caption="Spooky Mood in the Forest",
         description="full moon between the branches",
         created_at=fake.date_between(start_date='-1y', end_date='today'),
-        user_likes = sample[all_users, choice(range(0, 11))],
+        user_likes = sample(all_users, randint(0, 10)),
     )
     moon4 = Photo(
         author_id=4,
@@ -240,7 +240,7 @@ def seed_photos(all_users):
         caption="Conjunction of the Moon and The Pleiades",
         description="Captured with a Vixen VSD astrograph and a Nikon ZII, six 1250 .5 sec x 120.",
         created_at=fake.date_between(start_date='-1y', end_date='today'),
-        user_likes = sample[all_users, choice(range(0, 11))],
+        user_likes = sample(all_users, randint(0, 10)),
     )
     moon5 = Photo(
         author_id=4,
@@ -248,7 +248,7 @@ def seed_photos(all_users):
         caption="Waxing Crescent",
         description="I can't believe I finally caught one!! Yes!!!",
         created_at=fake.date_between(start_date='-1y', end_date='today'),
-        user_likes = sample[all_users, choice(range(0, 11))],
+        user_likes = sample(all_users, randint(0, 10)),
     )
 
 
