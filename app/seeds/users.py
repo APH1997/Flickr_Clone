853f19775346e47ed9a518e4f889fdd2972e934c
Andre <andrehristu@gmail.com>
2023-06-09 00:18:16 -0400

    spruced up for graduation :)

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -1,29 +1,44 @@
 from app.models import db, User, environment, SCHEMA
 from sqlalchemy.sql import text
-
+from random import choice
 
 # Adds a demo user, you can add other users here if you want
 def seed_users():
+
+    proPics = [
+        "https://highrme-pics.s3.us-east-2.amazonaws.com/Screenshot+2023-06-08+at+11.54.45+PM.png",
+        "https://highrme-pics.s3.us-east-2.amazonaws.com/Screenshot+2023-06-08+at+11.55.01+PM.png",
+        "https://highrme-pics.s3.us-east-2.amazonaws.com/Screenshot+2023-06-08+at+11.55.13+PM.png",
+        "https://highrme-pics.s3.us-east-2.amazonaws.com/Screenshot+2023-06-08+at+11.56.21+PM.png",
+        "https://highrme-pics.s3.us-east-2.amazonaws.com/Screenshot+2023-06-08+at+11.55.42+PM.png",
+        "https://highrme-pics.s3.us-east-2.amazonaws.com/Screenshot+2023-06-08+at+11.57.16+PM.png",
+        "https://highrme-pics.s3.us-east-2.amazonaws.com/Screenshot+2023-06-08+at+11.57.06+PM.png",
+        "https://highrme-pics.s3.us-east-2.amazonaws.com/Screenshot+2023-06-08+at+11.57.55+PM.png",
+        "https://highrme-pics.s3.us-east-2.amazonaws.com/Screenshot+2023-06-08+at+11.57.24+PM.png",
+        "https://highrme-pics.s3.us-east-2.amazonaws.com/Screenshot+2023-06-08+at+11.58.29+PM.png",
+        "https://highrme-pics.s3.us-east-2.amazonaws.com/Screenshot+2023-06-08+at+11.58.10+PM.png",
+        "https://highrme-pics.s3.us-east-2.amazonaws.com/Screenshot+2023-06-08+at+11.58.49+PM.png"
+    ]
     demo = User(
-        username='Demo', first_name='Demo', last_name='Lition', email='demo@aa.io', password='password')
+        username='Demo', first_name='Demo', last_name='Lition', email='demo@aa.io', password='password', profile_image_url=choice(proPics))
     marnie = User(
-        username='SunsetSnapper', first_name='Marnothy', last_name='Glumpford IV', email='marnie@aa.io', password='password')
+        username='SunsetSnapper', first_name='Marnothy', last_name='Glumpford IV', email='marnie@aa.io', password='password', profile_image_url=choice(proPics))
     bobbie = User(
-        username='GalacticGawker', first_name='Robert', last_name='Szabo', email='bobbie@aa.io', password='password')
+        username='GalacticGawker', first_name='Robert', last_name='Szabo', email='bobbie@aa.io', password='password', profile_image_url=choice(proPics))
     luna = User(
-        username='LunarLunatic', first_name='Luthor', last_name='Ludlowe', email='luthor@aa.io', password='password')
+        username='LunarLunatic', first_name='Luthor', last_name='Ludlowe', email='luthor@aa.io', password='password', profile_image_url=choice(proPics))
     clouds = User(
-        username='CloudConniseur', first_name='Barnaby', last_name='Klaxton', email='barnaby@aa.io', password='password')
+        username='CloudConniseur', first_name='Barnaby', last_name='Klaxton', email='barnaby@aa.io', password='password', profile_image_url=choice(proPics))
     padding2 = User(
-        username='HubbleBubbleTrubble', first_name='Dirk', last_name='Samson', email='dirk@aa.io', password='password')
+        username='HubbleBubbleTrubble', first_name='Dirk', last_name='Samson', email='dirk@aa.io', password='password', profile_image_url=choice(proPics))
     padding3 = User(
-        username='xXDragonGodXx68', first_name='Dorian', last_name='Macias', email='dorian@aa.io', password='password')
+        username='xXDragonGodXx68', first_name='Dorian', last_name='Macias', email='dorian@aa.io', password='password', profile_image_url=choice(proPics))
     padding4 = User(
-        username='ButterBallBandit', first_name='Skylar', last_name='Biggums', email='skylar@aa.io', password='password')
+        username='ButterBallBandit', first_name='Skylar', last_name='Biggums', email='skylar@aa.io', password='password', profile_image_url=choice(proPics))
     padding5 = User(
-        username='SnatchMan', first_name='Ron', last_name='Bossman', email='ron@aa.io', password='password')
+        username='SnatchMan', first_name='Ron', last_name='Bossman', email='ron@aa.io', password='password', profile_image_url=choice(proPics))
     padding6 = User(
-        username='GlueEater1122', first_name='Gloria', last_name='Kaufmann', email='gloria@aa.io', password='password')
+        username='GlueEater1122', first_name='Gloria', last_name='Kaufmann', email='gloria@aa.io', password='password', profile_image_url=choice(proPics))
 
     allUsers = [demo, marnie, bobbie, luna, clouds, padding2,
                 padding3, padding4, padding5, padding6]
