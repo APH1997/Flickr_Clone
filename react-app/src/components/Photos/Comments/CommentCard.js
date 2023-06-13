e52d3d7b7d8e8a62b250a46fc0ee0629b15fd1c9
Andre <andrehristu@gmail.com>
2023-06-13 12:02:03 -0400

    reply card styling

--- react-app/src/components/Photos/Comments/CommentCard.js
+++ react-app/src/components/Photos/Comments/CommentCard.js
@@ -7,8 +7,6 @@ import { useState } from "react"
 import EditComment from "./EditCommentForm"
 import ReplyCard from "./ReplyCard"
 
-
-
 function CommentCard({ comment }) {
     const user = useSelector((state) => state.session.user)
     const [showReplies, setShowReplies] = useState(false)
@@ -17,45 +15,45 @@ function CommentCard({ comment }) {
 
     return (
         <>
-        <div className="comment-card-container">
-            <div className="comment-card-content-container">
-                <div>
-                    <img id="comment-card-pro-pic" src={comment.author.profile_picture_url}></img>
+            <div className="comment-card-container">
+                <div className="comment-card-content-container">
+                    <div>
+                        <img id="comment-card-pro-pic" src={comment.author.profile_picture_url}></img>
 
-                </div>
-                <div className="comment-card-content">
-                    <NavLink to={`/users/${comment.author.id}`}>{comment.author.first_name} {comment.author.last_name}</NavLink>
-                    <div>
-                        {!isEditing && comment.content}
-                        {isEditing && <EditComment content={comment} setIsEditing={setIsEditing}/>}
                     </div>
-                    <div onClick={() => setShowReplies(!showReplies)}>
-                        {comment.replies.length} <i className="far fa-comments"></i>
+                    <div className="comment-card-content">
+                        <NavLink to={`/users/${comment.author.id}`}>{comment.author.first_name} {comment.author.last_name}</NavLink>
+                        <div>
+                            {!isEditing && comment.content}
+                            {isEditing && <EditComment content={comment} setIsEditing={setIsEditing} />}
+                        </div>
+                        <div onClick={() => setShowReplies(!showReplies)}>
+                            {comment.replies.length} <i className="far fa-comments"></i>
+                        </div>
                     </div>
                 </div>
+                {user.id === comment.author.id &&
+                    <div className="delete-comment-modal-button">
+                        <div>
+                            <button>
+                                <i className="fas fa-edit" onClick={() => setIsEditing(!isEditing)}></i>
+                            </button>
+                            <OpenModalButton
+                                buttonText={<i className="fas fa-trash-alt"></i>}
+                                modalComponent={
+                                    <DeleteComment
+                                        commentId={comment.id}
+                                        photoId={photo.id}
+                                    />
+                                }
+                            />
+                        </div>
+                    </div>
+                }
             </div>
-            {user.id === comment.author.id &&
-                <div className="delete-comment-modal-button">
-                    <div>
-                        <button>
-                            <i className="fas fa-edit" onClick={() => setIsEditing(!isEditing)}></i>
-                        </button>
-                        <OpenModalButton
-                            buttonText={<i className="fas fa-trash-alt"></i>}
-                            modalComponent={
-                                <DeleteComment
-                                    commentId={comment.id}
-                                    photoId={photo.id}
-                                />
-                            }
-                        />
-                    </div>
-                </div>
-            }
-        </div>
-        {showReplies &&
+            {showReplies &&
                 comment.replies.map((reply) =>
-                    <ReplyCard />)}
+                    <ReplyCard reply={reply} />)}
         </>
     )
 }
--- react-app/src/components/Photos/Comments/ReplyCard.js
+++ react-app/src/components/Photos/Comments/ReplyCard.js
@@ -1,8 +1,22 @@
-import CommentCard from "./CommentCard";
+import { NavLink } from "react-router-dom"
 
-function ReplyCard(){
+function ReplyCard({ reply }) {
     return (
-        <div>Reply!</div>
+        <div className="comment-card-container reply-container">
+            <div className="comment-card-content-container">
+                <div>
+                    <img id="comment-card-pro-pic" src={reply.author.profile_picture_url}></img>
+                </div>
+                <div className="comment-card-content">
+                    <NavLink to={`/users/${reply.author.id}`}>{reply.author.first_name} {reply.author.last_name}</NavLink>
+                    <div>
+                        {reply.content}
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
     )
 }
 
