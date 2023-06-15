53743e4459922f3f6abd1cb2cc8af6a7a3f9bc81
Andre <andrehristu@gmail.com>
2023-06-15 10:45:40 -0400

    passing allPhotos into thunk on photo by id

--- react-app/src/components/Photos/PhotoDetails/index.js
+++ react-app/src/components/Photos/PhotoDetails/index.js
@@ -24,7 +24,7 @@ function PhotoDetails(){
       }, [])
 
     useEffect(() => {
-        dispatch(getAllPhotosThunk())
+        dispatch(getAllPhotosThunk(allPhotos))
     },[dispatch])
 
     if (!allPhotos) return <Loader/>;
