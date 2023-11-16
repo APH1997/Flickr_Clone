198625a0aecf0b5982dfc4419304231d5ff8f672
Andre <andrehristu@gmail.com>
2023-11-16 12:47:45 -0500

    basic unauthorized tests for user endpoints

--- app/tests/test_api.py
+++ app/tests/test_api.py
@@ -11,12 +11,20 @@ class TestUnauthorized:
     """
     @pytest.mark.parametrize('endpoint', [
         '/users/',
-        '/users/1',
+        '/users/10',
         ])
     def test_get_users(self, endpoint):
         response = requests.get(f'{APIROOT}{endpoint}')
         assert response.status_code == 401
 
     def test_edit_user(self):
-        response = requests.put(f'{APIROOT}/users/1/edit')
+        response = requests.put(f'{APIROOT}/users/10/edit')
         assert response.status_code == 401
+    # TODO: authorized with invalid data
+    # TODO: authorized but not as targeted user
+
+    def test_bio_edit(self):
+        response = requests.put(f'{APIROOT}/users/10/bio/edit')
+        assert response.status_code == 401
+    # TODO: authorized with invalid data
+    # TODO: authorized but not as targeted user
