2ffd0ee0e428942638e1cd1569d2baf91f2b691a
Andre <andrehristu@gmail.com>
2023-11-16 12:41:28 -0500

    edit user test

--- app/tests/test_api.py
+++ app/tests/test_api.py
@@ -3,7 +3,20 @@ import requests
 
 APIROOT = 'http://127.0.0.1:5000/api'
 
+class TestUnauthorized:
+    """
+    This class of tests tries every api endpoint.
+    They should all give 401s because we have not
+    authorized a user
+    """
+    @pytest.mark.parametrize('endpoint', [
+        '/users/',
+        '/users/1',
+        ])
+    def test_get_users(self, endpoint):
+        response = requests.get(f'{APIROOT}{endpoint}')
+        assert response.status_code == 401
 
-def test_endpoint():
-    response = requests.get(f'{APIROOT}/users/1')
-    assert response.status_code == 401
+    def test_edit_user(self):
+        response = requests.put(f'{APIROOT}/users/1/edit')
+        assert response.status_code == 401
